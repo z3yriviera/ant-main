390fe498c8c2dda5840e47b46aa28b6e27b4c375
z3yriviera <zey-zeyy@yandex.com>
2025-09-01 10:23:21 +0300

    .

--- src/i18n.js
+++ src/i18n.js
@@ -3,36 +3,12 @@ import { initReactI18next } from "react-i18next";
 
 i18n.use(initReactI18next).init({
   resources: {
-    en: {
-      translation: {
-        welcome: "Welcome Back!",
-        email: "Email",
-        password: "Password",
-        login: "Login",
-        signup: "Sign Up",
-        home: "Home Page",
-        logout: "Logout",
-        createAccount: "Create Your Account"
-      },
-    },
-    tr: {
-      translation: {
-        welcome: "Tekrar Hoş Geldiniz!",
-        email: "E-posta",
-        password: "Şifre",
-        login: "Giriş Yap",
-        signup: "Kayıt Ol",
-        home: "Ana Sayfa",
-        logout: "Çıkış Yap",
-        createAccount: "Hesabınızı Oluşturun"
-      },
-    },
+    en: { translation: { home: "Home", logout: "Logout" } },
+    tr: { translation: { home: "Ana Sayfa", logout: "Çıkış Yap" } },
   },
-  lng: "tr", // Varsayılan dil
+  lng: "tr",
   fallbackLng: "en",
-  interpolation: {
-    escapeValue: false,
-  },
+  interpolation: { escapeValue: false },
 });
 
 export default i18n;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "antd/dist/reset.css";
-import "./i18n"; // i18n ekledik
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
