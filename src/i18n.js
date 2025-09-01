ddd6fa22980040faca889b467d053ea307474138
z3yriviera <zey-zeyy@yandex.com>
2025-09-01 10:09:15 +0300

    ..

--- src/i18n.js
+++ src/i18n.js
@@ -1,44 +1,38 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
-const resources = {
-  en: {
-    translation: {
-      welcome: "Welcome Back!",
-      login: "Login",
-      signup: "Sign Up",
-      email: "Email",
-      password: "Password",
-      confirmPassword: "Confirm Password",
-      fullName: "Full Name",
-      logout: "Logout",
-      darkMode: "Dark Mode",
-      lightMode: "Light Mode",
-      or: "or",
-    },
-  },
-  tr: {
-    translation: {
-      welcome: "Tekrar Hoşgeldiniz!",
-      login: "Giriş Yap",
-      signup: "Kayıt Ol",
-      email: "E-posta",
-      password: "Şifre",
-      confirmPassword: "Şifreyi Onayla",
-      fullName: "Ad Soyad",
-      logout: "Çıkış Yap",
-      darkMode: "Karanlık Mod",
-      lightMode: "Aydınlık Mod",
-      or: "veya",
-    },
-  },
-};
-
 i18n.use(initReactI18next).init({
-  resources,
-  lng: "en",
+  resources: {
+    en: {
+      translation: {
+        welcome: "Welcome Back!",
+        email: "Email",
+        password: "Password",
+        login: "Login",
+        signup: "Sign Up",
+        home: "Home Page",
+        logout: "Logout",
+        createAccount: "Create Your Account"
+      },
+    },
+    tr: {
+      translation: {
+        welcome: "Tekrar Hoş Geldiniz!",
+        email: "E-posta",
+        password: "Şifre",
+        login: "Giriş Yap",
+        signup: "Kayıt Ol",
+        home: "Ana Sayfa",
+        logout: "Çıkış Yap",
+        createAccount: "Hesabınızı Oluşturun"
+      },
+    },
+  },
+  lng: "tr", // Varsayılan dil
   fallbackLng: "en",
-  interpolation: { escapeValue: false },
+  interpolation: {
+    escapeValue: false,
+  },
 });
 
 export default i18n;
